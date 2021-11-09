97b73c0df098e8dac6d8613309531bd186c1d137
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2021-11-09 07:05:47 -0300

    fix on textarea

--- src/components/Textarea/styles.tsx
+++ src/components/Textarea/styles.tsx
@@ -17,7 +17,7 @@ const outlined = css`
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  ${({ width }) => (width ? `width: ${width}` : 'width: 100%')};
+  width: 100%;
 `
 
 export const InputContainer = styled.div.attrs(
