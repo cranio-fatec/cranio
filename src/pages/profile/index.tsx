9e7d919ce25818a99c1d305436cf7f75fe888502
Bruno Rodrigues <brunorodriguesricardo@gmail.com>
2022-12-05 02:23:00 -0300

    Profile message

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 // import { Container } from './styles';
 
 const Profile: React.FC = () => {
-	return <div />
+	return <div>An error has ocurred.</div>
 }
 
 export default Profile
